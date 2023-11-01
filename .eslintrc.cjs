6c0fea1481085eb06f0ab1abdbce190137361da8
sergerdn <64213648+sergerdn@users.noreply.github.com>
2023-10-31 23:56:32 -0700

    feat: add script to parse csv data

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,29 +3,30 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: 'airbnb-base',
+  extends: "airbnb-base",
   overrides: [
     {
       env: {
         node: true
       },
       files: [
-        '.eslintrc.{js,cjs}'
+        ".eslintrc.{js,cjs}"
       ],
       parserOptions: {
-        sourceType: 'script'
+        sourceType: "script"
       }
     }
   ],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaVersion: "latest",
+    sourceType: "module"
   },
-  'ignorePatterns': [
-    'bas_scripts/modules/compiled/AccountProfileMaker/AccountProfileMaker/*.js',
+  "ignorePatterns": [
+    "bas_scripts/modules/compiled/AccountProfileMaker/AccountProfileMaker/*.js"
   ],
   rules: {
-    'no-console': 'off', // Allow the use of console.log
-    semi: ['error', 'never'] // Disallow semicolons as the end of statements
+    "no-console": "off", // Allow the use of console.log
+    semi: ["error", "never"], // Disallow semicolons as the end of statements,
+    "comma-dangle": "off" // Disallow trailing commas in an object and array literals
   }
 };
