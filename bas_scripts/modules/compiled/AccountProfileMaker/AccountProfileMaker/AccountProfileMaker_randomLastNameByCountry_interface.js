4514fcc8cef02717871099dacf54fb2e223a526e
sergerdn <64213648+sergerdn@users.noreply.github.com>
2023-11-01 20:41:43 +0300

    feat: add last name (#12)

---
+++ bas_scripts/modules/compiled/AccountProfileMaker/AccountProfileMaker/AccountProfileMaker_randomLastNameByCountry_interface.js
@@ -0,0 +1,9 @@
+<div class="container-fluid">
+<%= _.template($('#input_constructor').html())({id:"icpoqeww", description:"country", default_selector: "string", disable_int:true, variants: ["US","RU"], value_string: "US", help: {description: "The ISO code of the country (e.g., 'US' for the United States)."} }) %>
+<%= _.template($('#input_constructor').html())({id:"wtfyeyro", description:"gender", default_selector: "string", disable_expression:true, disable_int:true, variants: ["male","female"], value_string: "male", help: {description: "The gender ('male' or 'female') for which to generate a name."} }) %>
+<%= _.template($('#variable_constructor').html())({id:"Save", description:"Result", default_variable: "APM_LAST_NAME_RESULT", help: {description: "A random last name based on the provided country and gender."}}) %>
+</div>
+<div class="tooltipinternal">
+<div class="tr tooltip-paragraph-first-fold">Generates a random name based on the specified country and gender.</div>
+</div>
+<%= _.template($('#back').html())({action:"executeandadd", visible:true}) %>
