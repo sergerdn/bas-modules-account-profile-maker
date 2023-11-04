caa2aca5de0773bd53af664131622710011913b4
sergerdn <64213648+sergerdn@users.noreply.github.com>
2023-11-04 08:31:31 -0700

    chore: format code of module

--- bas_scripts/modules/compiled/AccountProfileMaker/AccountProfileMaker/AccountProfileMaker_generateLastPassStyleUsername_interface.js
+++ bas_scripts/modules/compiled/AccountProfileMaker/AccountProfileMaker/AccountProfileMaker_generateLastPassStyleUsername_interface.js
@@ -1,8 +1,31 @@
 <div class="container-fluid">
-<%= _.template($('#input_constructor').html())({id:"dvdwtiih", description:"length", default_selector: "int", disable_expression:true, disable_string:true, value_number: 15, min_number:-999999, max_number:999999, help: {description: "The desired length of the username. The minimum length is 10. If you provide fewer characters, it will be set to 10."} }) %>
-<%= _.template($('#variable_constructor').html())({id:"Save", description:"Result", default_variable: "APM_USERNAME_RESULT", help: {description: "A pronounceable username of the specified length."}}) %>
+    <%= _.template($('#input_constructor')
+            .html())({
+        id: "dvdwtiih",
+        description: "length",
+        default_selector: "int",
+        disable_expression: true,
+        disable_string: true,
+        value_number: 15,
+        min_number: -999999,
+        max_number: 999999,
+        help: { description: "The desired length of the username. The minimum length is 10. If you provide fewer characters, it will be set to 10." }
+    }) %>
+    <%= _.template($('#variable_constructor')
+            .html())({
+        id: "Save",
+        description: "Result",
+        default_variable: "APM_USERNAME_RESULT",
+        help: { description: "A pronounceable username of the specified length." }
+    }) %>
 </div>
 <div class="tooltipinternal">
-<div class="tr tooltip-paragraph-first-fold">Generates a LastPass-like pronounceable username, ensuring it meets a specified minimum length by utilizing weighted transitions between letter pairs for natural phonetic flow.</div>
+    <div class="tr tooltip-paragraph-first-fold">Generates a LastPass-like pronounceable username, ensuring it meets a
+        specified minimum length by utilizing weighted transitions between letter pairs for natural phonetic flow.
+    </div>
 </div>
-<%= _.template($('#back').html())({action:"executeandadd", visible:true}) %>
+<%= _.template($('#back')
+        .html())({
+    action: "executeandadd",
+    visible: true
+}) %>
--- bas_scripts/modules/compiled/AccountProfileMaker/AccountProfileMaker/AccountProfileMaker_randomFirstNameByCountry_interface.js
+++ bas_scripts/modules/compiled/AccountProfileMaker/AccountProfileMaker/AccountProfileMaker_randomFirstNameByCountry_interface.js
@@ -1,9 +1,38 @@
 <div class="container-fluid">
-<%= _.template($('#input_constructor').html())({id:"ecoezlmo", description:"country", default_selector: "string", disable_expression:true, disable_int:true, value_string: "US", help: {description: "The ISO code of the country (e.g., 'US' for the United States). Currently supported countries are US and RU."} }) %>
-<%= _.template($('#input_constructor').html())({id:"lbggowju", description:"gender", default_selector: "string", disable_expression:true, disable_int:true, value_string: "male", help: {description: "The gender ('male' or 'female') for which to generate a name."} }) %>
-<%= _.template($('#variable_constructor').html())({id:"Save", description:"Result", default_variable: "APM_FIRST_NAME_RESULT", help: {description: "A random name based on the provided country and gender."}}) %>
+    <%= _.template($('#input_constructor')
+            .html())({
+        id: "ecoezlmo",
+        description: "country",
+        default_selector: "string",
+        disable_expression: true,
+        disable_int: true,
+        value_string: "US",
+        help: { description: "The ISO code of the country (e.g., 'US' for the United States). Currently supported countries are US and RU." }
+    }) %>
+    <%= _.template($('#input_constructor')
+            .html())({
+        id: "lbggowju",
+        description: "gender",
+        default_selector: "string",
+        disable_expression: true,
+        disable_int: true,
+        value_string: "male",
+        help: { description: "The gender ('male' or 'female') for which to generate a name." }
+    }) %>
+    <%= _.template($('#variable_constructor')
+            .html())({
+        id: "Save",
+        description: "Result",
+        default_variable: "APM_FIRST_NAME_RESULT",
+        help: { description: "A random name based on the provided country and gender." }
+    }) %>
 </div>
 <div class="tooltipinternal">
-<div class="tr tooltip-paragraph-first-fold">Generates a random name based on the specified country and gender.</div>
+    <div class="tr tooltip-paragraph-first-fold">Generates a random name based on the specified country and gender.
+    </div>
 </div>
-<%= _.template($('#back').html())({action:"executeandadd", visible:true}) %>
+<%= _.template($('#back')
+        .html())({
+    action: "executeandadd",
+    visible: true
+}) %>
